dacf9d624b97722aeedc46408ccc6e1a924d7fca
Eder Negrete <noe.villegas.contractor@bbva.com>
2016-11-11 17:08:22 -0600

    feat(all): add auth.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var cards = require('./routes/cards');
+var auth = require('./routes/auth');
 
 var app = express();
 
@@ -24,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/cards', cards);
-
+app.use('/auth', auth);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
---
+++ routes/auth.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+router.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Request-Method', 'POST');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, token, Accept');
+  res.header('Access-Control-Expose-Headers', 'token');
+  res.header('token', '5715261762');
+  next();
+});
+
+router.post('/v01', function(req, res){
+  res.json({
+    authenticationState: 'OK'
+  });
+});
+
+module.exports = router;
