e7227424b636c5707afd59da5967b64ee1fb59fc
Eder Negrete <noe.villegas.contractor@bbva.com>
2016-11-14 11:44:15 -0600

    fix(cards.js): add headers cards.js

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ var router = express.Router();
 router.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Request-Method', 'POST');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, token, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Expose-Headers', 'token');
   res.header('token', '5715261762');
   next();
--- routes/cards.js
+++ routes/cards.js
@@ -2,6 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+router.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 router.get('/list', function(req, res, next) {
   res.json({
     cards: [
