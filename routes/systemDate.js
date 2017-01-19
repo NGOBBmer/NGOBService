fec89556a79c6ecb8f8876b6a4bd1122e4e9455a
xmz2488 <giovanni.nicolas.contractor@bbva.com>
2017-01-18 18:49:18 -0600

    Fix (systemDate.js): update datetime

--- routes/systemDate.js
+++ routes/systemDate.js
@@ -2,20 +2,36 @@ var express = require('express');
 var router = express.Router();
 var dates01 = require('../mock/V00/users/systemDate/date01.json');
 
-function getDate(){
-  var now = new Date();
-  return now;
+function getDate(now) {
+   var yyyy = now.getFullYear();
+   var mm = now.getMonth()+1;
+   var dd  = now.getDate();
+
+   return String(10000*yyyy + 100*mm + dd); 
+}
+
+function getTime(now){
+  var hh = now.getHours();
+  var mm = now.getMinutes();
+  var ss = now.getSeconds();
+  var ms = now.getMilliseconds();
+
+  hh < 10 ? hh = '0' + hh : hh = hh;
+  mm < 10 ? mm = '0' + mm : mm = mm;
+  ss < 10 ? ss = '0' + ss : ss = ss;
+
+  return hh + ":" +  mm + ":" + ss + "." +  ms;
 }
 
 function getJson(){
   var now = new Date();
-  var dt = JSON.stringify(now);
-  dt = dt.substring(1, dt.length -2);
+  var dt = getDate(now);
+  var newDate = dt.substring(0,4) + "-" + dt.substring(4,6) + "-" + dt.substring(6);
   var obj = { 
     "data": 
     { 
-      "date": dt.substring(0,10),
-      "time": dt.substring(11),
+      "date":  newDate,
+      "time": getTime(now),
       "timeZone": "-06:00"
     }
   }
