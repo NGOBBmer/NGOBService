253b26f18aad6a09c055cd9fedb3a753ab97b6c4
xmz2488 <giovanni.nicolas.contractor@bbva.com>
2017-01-25 11:47:31 -0600

    Feat (all): add news json for accountStatements service

--- routes/accountStatement.js
+++ routes/accountStatement.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var TCMXP0000001 = require('../mock/V00/cards/accountstatement/TCMXP0000001.json');
-var TCMXP0000001_next = require('../mock/V00/cards/accountstatement/TCMXP0000001_next.json');
-var TCMXP0000001_next2 = require('../mock/V00/cards/accountstatement/TCMXP0000001_next2.json');
-var TCMXP0000002 = require('../mock/V00/cards/accountstatement/TCMXP0000002.json');
-var TCMXP0000002_next = require('../mock/V00/cards/accountstatement/TCMXP0000002_next.json');
+var PERIOD0001 = require('../mock/V00/cards/accountstatement/PERIOD0001.json');
+var PERIOD0002 = require('../mock/V00/cards/accountstatement/PERIOD0002.json');
+var PERIOD0003 = require('../mock/V00/cards/accountstatement/PERIOD0003.json');
+var PERIOD0004 = require('../mock/V00/cards/accountstatement/PERIOD0004.json');
 var AS_NO_DATA = require('../mock/V00/cards/accountstatement/accountstatement_error_sinperiodos.json');
 var AS_ERROR_ID = require('../mock/V00/cards/accountstatement/accountstatement_error_id.json');
 
@@ -18,24 +17,22 @@ router.use(function(req, res, next) {
     next();
 });
 
-// handler for query http://localhost:3000/cards/V00/cards/TCMXP0000001/accountStatements?paginationKey=
-router.get('/V00/cards/:id/accountStatements/', function(req, res, next) {
+// handler for query http://localhost:3000/cards/V00/cards/TCMXP0000001/accountStatement?periodId=PERIOD0001&type=pdf
+router.get('/V00/cards/:id/accountStatement/', function(req, res, next) {
 	if(req.params && req.params.id){
-	    if (req.params && req.params.id == 'TCMXP0000001')  {
-        if (req.query.paginationKey != ""){
-          return res.json(TCMXP0000001_next);
-        }else{
-          return res.json(TCMXP0000001);
-        }
+	    if (req.params && req.query.periodId == 'PERIOD0001')  {
+        return res.json(PERIOD0001);
 	    }
-      if (req.params && req.params.id == 'TCMXP0000002')  {
-        if (req.query.paginationKey != ""){
-          return res.json(TCMXP0000002_next);
-        }else{
-          return res.json(TCMXP0000002);
-        }
+      if (req.params && req.query.periodId == 'PERIOD0002')  {
+        return res.json(PERIOD0002);
       }
-      if (req.params && req.params.id == 'TCMXP0000003')  {
+      if (req.params && req.query.periodId == 'PERIOD0003')  {
+        return res.json(PERIOD0003);
+      }
+      if (req.params && req.query.periodId == 'PERIOD0004')  {
+        return res.json(PERIOD0004);
+      }
+      if (req.params && req.params.id == 'PERIOD0005')  {
         return res.json(AS_NO_DATA);
       }
       return res.json(AS_ERROR_ID);
