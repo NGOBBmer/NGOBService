4e25024411494a0c25c73007dcdd3af2ab7916f0
Eder Gonzalez <ederyirnglz@gmail.com>
2017-03-06 16:03:26 -0600

    post test

--- routes/dashboard/tsec.js
+++ routes/dashboard/tsec.js
@@ -1,21 +1,31 @@
 var express = require('express');
+var app = express();
 var router = express.Router();
+var bodyParser = require('body-parser');
 var TSEC = require('../../mock/V00/dashboard/tsec/tsec_01.json');
 
 /* GET users listing. */
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 router.use(function(req, res, next) {
-    var host = req.get('origin');
-    res.setHeader('Access-Control-Allow-Origin', host||"*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,tsec');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('tsec', '654321')
-    next();
+  var host = req.get('origin');
+  res.setHeader('Access-Control-Allow-Origin', host || '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,tsec,Accept-Language');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('tsec', '654321');
+  next();
 });
 
+/*app.post('/V00/tsec', function(req, res, next) {
+  return res.json(TSEC);
+  next();
+});
+*/
 // handler for query http://localhost:4000/dashboard/V00/tsec
 router.post('/V00/tsec', function(req, res, next) {
-	return res.json(TSEC);
+  return res.json(TSEC);
   next();
 });
 
