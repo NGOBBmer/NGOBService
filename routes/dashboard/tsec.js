d18f458faf8399e833248b334260ff9c5bf24693
Eder Gonzalez <ederyirnglz@gmail.com>
2017-03-06 16:53:49 -0600

    post test

--- routes/dashboard/tsec.js
+++ routes/dashboard/tsec.js
@@ -15,6 +15,7 @@ router.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,tsec,Accept-Language');
   res.setHeader('Access-Control-Allow-Credentials', true);
   res.setHeader('Tsec', '654321');
+  res.header('Access-Control-Expose-Headers', 'Tsec');
   next();
 });
 
@@ -25,7 +26,7 @@ router.use(function(req, res, next) {
 */
 // handler for query http://localhost:4000/dashboard/V00/tsec
 router.post('/V00/tsec', function(req, res, next) {
-  return res.header('Access-Control-Expose-Headers', 'tsec');
+  return {};
   next();
 });
 
