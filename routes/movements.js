e26afef22c8835aee4f386f3439312b2c635fa8c
xmz2488 <giovanni.nicolas.contractor@bbva.com>
2017-03-09 14:24:24 -0600

    Fix (all): movs ok

--- routes/movements.js
+++ routes/movements.js
@@ -85,13 +85,13 @@ router.get('/V00/cards/movements/:id', function(req, res, next) {
         }
         if (req.params.id == 'TCMXP0000004'){
              if (req.query.paginationKey && req.query.initialDate && req.query.finalDate) {
-                return res.json(movements_default);
+                return res.json(movements_TJ0001_dates_next);
             } else if (req.query.initialDate && req.query.finalDate) {
-                return res.json(movements_default);
+                return res.json(movements_TJ0004_next);
             }else if (req.query.paginationKey) {
-                return res.json(movements_default);
+                return res.json(movements_TJ0004_next);
             } else{
-                return res.json(movements_default);
+                return res.json(movements_TJ0004);
             }
         }
         if (req.params.id == 'TCMXP0000005'){
