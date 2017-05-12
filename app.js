65a86a5c86e7a70602aa53d4f09e6a85397bd232
Paola Chavez <Paola@MacBook-Pro-de-Gonet.local>
2017-05-12 12:29:15 -0500

    Feat (all): new example json i want menu

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ var dashboard = require('./routes/dashboard/dashboard');
 var paola = require('./routes/paola/paola');
 var assets = require('./routes/assets');
 var auth = require('./routes/auth');
+var iWantMenu = require('./routes/dashboard/iWantMenu');
 
 var app = express();
 
@@ -73,6 +74,7 @@ app.use('/dashboard', banners);
 app.use('/paola', paola);
 app.use('/auth', auth);
 app.use('/img', assets);
+app.use('/dashboard', iWantMenu);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
