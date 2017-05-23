d46670f716248817ef6129d67787859a68ce4f59
xmz2488 <giovanni.nicolas.contractor@bbva.com>
2017-05-23 17:54:07 -0500

    Fix (all): add new json dashboards and modify campaigns

--- routes/dashboard/dashboard.js
+++ routes/dashboard/dashboard.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var dashboard_01 = require('../../mock/V00/dashboard/dashboard/dashboard_01.json');
+var dashboard_02 = require('../../mock/V00/dashboard/dashboard/dashboard_02.json');
+
 // var dashboard_error = require('../../mock/V00/dashboard/dashboard/dashboard_error.json');
 
 /* GET users listing. */
@@ -18,9 +20,13 @@ router.use(function(req, res, next) {
 
 // handler for query http://localhost:5000/dashboard/V00/dashboard
 router.get('/V00/dashboard', function(req, res, next) {
-	return res.json(dashboard_01);
+    var tsec = req.headers['tsec'];
+    if (tsec == null || tsec == '')
+	   return res.json(dashboard_02);
+    else
+        return res.json(dashboard_01);
   next();
 });
-
+4152313000466834
 
 module.exports = router;
