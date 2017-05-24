732cbd49dcdea9c9d42e19bc858165c5cb6e3883
xmz2488 <giovanni.nicolas.contractor@bbva.com>
2017-05-24 13:55:37 -0500

    Fix (all): add new json dashboards

--- routes/dashboard/dashboard.js
+++ routes/dashboard/dashboard.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var dashboard_01 = require('../../mock/V00/dashboard/dashboard/dashboard_01.json');
 var dashboard_02 = require('../../mock/V00/dashboard/dashboard/dashboard_02.json');
+var dashboard_02_si = require('../../mock/V00/dashboard/dashboard/dashboard_02_si.json');
 
 var dashboard_error = require('../../mock/V00/dashboard/dashboard/dashboard_error.json');
 
@@ -22,8 +23,10 @@ router.use(function(req, res, next) {
 router.get('/V00/dashboard', function(req, res, next) {
     var tsec = req.headers['tsec'];
     console.log(req.query.$filter);
-    if ((tsec == null || tsec != undefined || tsec == '') && req.query.$filter === 'productType==TT')
+    if ((tsec == null || tsec != undefined || tsec == '') && req.query.$filter === 'productType==TT' )
 	   return res.json(dashboard_02);
+    else if ((tsec == null || tsec != undefined || tsec == '') && req.query.$filter === 'productType==TT,contract==00743616562092377152')
+        return res.json(dashboard_02_si);
     else if ((tsec != null || tsec != undefined || tsec != '') && req.query.$filter === 'productType==TT')
         return res.json(dashboard_01);
 
