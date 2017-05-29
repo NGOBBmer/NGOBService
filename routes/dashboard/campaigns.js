fd9f0cb5035ad75a44d3f5a59d900bd446027da4
xmz2488 <giovanni.nicolas.contractor@bbva.com>
2017-05-29 17:23:51 -0500

    Fix (all) change to campaigns with conditions and new service agileOperations

--- routes/dashboard/campaigns.js
+++ routes/dashboard/campaigns.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var CAMPANIA = require('../../mock/V00/dashboard/campaigns/campaigns_01.json');
+var CAMPANIA01 = require('../../mock/V00/dashboard/campaigns/campaigns_01.json');
+var CAMPANIA02 = require('../../mock/V00/dashboard/campaigns/campaigns_02.json');
+var CAMPANIA_error = require('../../mock/V00/dashboard/campaigns/campaigns_error.json');
+var CAMPANIA_error01 = require('../../mock/V00/dashboard/campaigns/campaigns_error01.json');
 
 /* GET users listing. */
 router.use(function(req, res, next) {
@@ -15,10 +18,15 @@ router.use(function(req, res, next) {
 
 // handler for query http://localhost:4000/dashboard/V00/campaigns
 router.get('/V00/campaigns', function(req, res, next) {
-	if(req.query){
-		return res.json(CAMPANIA);
-	}
-	return res.json(CAMPANIA);
+    var tsec = req.headers['tsec'];
+    if (tsec === '123456')
+		return res.json(CAMPANIA01);
+	else if (tsec === '654321')
+        return res.json(CAMPANIA02);
+    else if (tsec === '123654')
+        return res.status(400).json(CAMPANIA_error01);
+
+	return res.status(500).json(CAMPANIA_error);
   next();
 });
 
---
+++ routes/operations/agileOperations.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+var LISTA_ALL = require('../../mock/V00/operations/agileOperations/lista_all.json');
+var LISTA_PR = require('../../mock/V00/operations/agileOperations/lista_pr.json');
+var LISTA_RP = require('../../mock/V00/operations/agileOperations/lista_rp.json');
+var ERROR = require('../../mock/V00/operations/agileOperations/error.json');
+
+router.use(function(req, res, next) {
+  var host = req.get('origin');
+  res.setHeader('Access-Control-Allow-Origin', host || '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,tsec');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
+// handler for query http://localhost:4000/operations/V00/agileOperations?$filter=(businessFlow==ALL)
+router.get('/V00/agileOperations', function(req, res, next) {
+	console.log(req.query.$filter);
+  if (req.query.$filter === '(businessFlow==ALL)')
+  	return res.json(LISTA_ALL);
+  else if (req.query.$filter === '(businessFlow==RECURRING)')
+  	return res.json(LISTA_PR);
+  else if (req.query.$filter === '(businessFlow==FAST)')
+  	return res.json(LISTA_RP);
+
+  return res.json(ERROR);
+  next();
+});
+
+module.exports = router;
