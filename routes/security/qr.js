764ee9cb88ec36e0fb0e401b7a2de8238c357f28
xmz2488 <giovanni.nicolas.contractor@bbva.com>
2017-05-29 13:08:39 -0500

    Fix (all) add link to getQR

--- routes/security/qr.js
+++ routes/security/qr.js
@@ -6,8 +6,8 @@ var QR_ERROR = require('../../mock/V00/security/qr/qr_error.json');
 
 router.use(function(req, res, next) {
   var host = req.get('origin');
-  res.setHeader('Access-Control-Allow-Origin', host || '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS ');
+  res.setHeader('Access-Control-Allow-Origin', host||"*");
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,tsec');
   res.setHeader('Access-Control-Allow-Credentials', true);
   next();
@@ -28,9 +28,9 @@ router.post('/V00/getQR', function(req, res, next) {
     return res.json(QR_IMAGE);
   } else if (req.body.type=="REGISTER"){
     return res.status(400).json(QR_ERROR);
-  } else {
-    return res.status(500).json(QR_ERROR);
-  }
+  } 
+  
+  return res.status(500).json(QR_ERROR);
   
   next();
 });
