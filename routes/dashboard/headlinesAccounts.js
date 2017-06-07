cb61500521083a8f80e44bea808852fbe9f8a1ef
Paola Chavez <Paola@MacBook-Pro-de-Gonet.local>
2017-06-07 13:08:18 -0500

    Feat (all): new example json

---
+++ routes/dashboard/headlinesAccounts.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var DATA_01 = require('../../mock/V00/dashboard/headlinesAccounts/headlinesAccounts.json');
+var ERROR = require('../../mock/V00/dashboard/headlinesAccounts/headlinesAccountsError.json');
+
+/* GET users listing. */
+router.use(function(req, res, next) {
+    var host = req.get('origin');
+    res.setHeader('Access-Control-Allow-Origin', host||"*");
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,tsec');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+
+// handler for query http://localhost:4000/dashboard/V00/dashboard/headlinesAccounts
+router.get('/V00/dashboard/headlinesAccounts', function(req, res, next) {
+  if(req.query){
+    return res.json(DATA_01);
+  }
+  return res.status(400).json(ERROR);
+  next();
+});
+
+module.exports = router;
