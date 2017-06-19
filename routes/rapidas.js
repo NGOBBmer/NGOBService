346c7d5f8ec55918e645cd79a20aa9fdd9c9cf96
xmz2488 <giovanni.nicolas.contractor@bbva.com>
2017-06-19 17:09:26 -0500

    Fix (all): change rapids operations

--- routes/rapidas.js
+++ routes/rapidas.js
@@ -1,26 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.use(function(req, res, next) {
-    var host = req.get('origin');
-    res.setHeader('Access-Control-Allow-Origin', host||"*");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,tsec');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
-
 router.get('/rapidas/:id', function(req, res, next) {
 	if(req.params.id === "rapida_ob_40_x_40.png"){
-	  res.sendFile(__dirname + '/img/rapidas/rapida_ob_40_x_40.png');
+	  res.sendFile(__dirname + "/img/rapidas/rapida_ob_40_x_40.png");
 	}else if(req.params.id === "rapida_tt_40_x_40.png"){
-	  res.sendFile(__dirname + '/img/rapidas/rapida_tt_40_x_40.png');
+	  res.sendFile(__dirname + "/img/rapidas/rapida_tt_40_x_40.png");
 	}else if(req.params.id === "rapida_ta_40_x_40.png"){
-	  res.sendFile(__dirname + '/img/rapidas/rapida_ta_40_x_40.png');
+	  res.sendFile(__dirname + "/img/rapidas/rapida_ta_40_x_40.png");
 	}
-	res.sendFile(__dirname + '/img/rapidas/rapida_tt_40_x_40.png');
+	res.sendFile(__dirname + "/img/rapidas/rapida_tt_40_x_40.png");
 });
 
 
