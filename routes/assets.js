f78a41b6ec6492250892cd915fa8da0a0f59a4ab
xmz2488 <giovanni.nicolas.contractor@bbva.com>
2017-06-19 16:35:19 -0500

    Fix (all): change rapids all

--- routes/assets.js
+++ routes/assets.js
@@ -34,11 +34,11 @@ router.get('/ngob/instrumento/cronto/:id', function(req, res, next) {
 
 router.get('/rapidas/:id', function(req, res, next) {
 	if(req.params.id == "rapida_ob_40_x_40.png"){
-	  res.sendFile(__dirname + '/img/rapidas/rapida_ob_40_x_40.png');
+	  res.sendFile(__dirname + '/img/rapida/rapida_ob_40_x_40.png');
 	}else if(req.params.id == "rapida_tt_40_x_40.png"){
-	  res.sendFile(__dirname + '/img/rapidas/rapida_tt_40_x_40.png');
+	  res.sendFile(__dirname + '/img/rapida/rapida_tt_40_x_40.png');
 	}else if(req.params.id == "rapida_ta_40_x_40.png"){
-	  res.sendFile(__dirname + '/img/rapidas/rapida_ta_40_x_40.png');
+	  res.sendFile(__dirname + '/img/rapida/rapida_ta_40_x_40.png');
 	}
   res.sendFile(__dirname + '/img/iconos/check_green.svg');
 });
