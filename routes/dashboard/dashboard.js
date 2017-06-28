5d4feb8d0209f9870ccabfd35d5d0037da884751
xmz2488 <giovanni.nicolas.contractor@bbva.com>
2017-06-28 12:47:51 -0500

    Fix (all): service for investment

--- routes/dashboard/dashboard.js
+++ routes/dashboard/dashboard.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var dashboard_01 = require('../../mock/V00/dashboard/dashboard/dashboard_01.json');
 var dashboard_02 = require('../../mock/V00/dashboard/dashboard/dashboard_02.json');
-var dashboard_02_si = require('../../mock/V00/dashboard/dashboard/dashboard_02_si.json');
-
+var dashboard_02_SI = require('../../mock/V00/dashboard/dashboard/dashboard_02_si.json');
+var dashboard_03_IN = require('../../mock/V00/dashboard/dashboard/dashboard_03_in.json');
 var dashboard_error = require('../../mock/V00/dashboard/dashboard/dashboard_error.json');
 
 /* GET users listing. */
@@ -22,10 +22,13 @@ router.use(function(req, res, next) {
 // handler for query http://localhost:5000/dashboard/V00/dashboard?$filter=productType==TT
 router.get('/V00/dashboard', function(req, res, next) {
     var tsec = req.headers['tsec'];
+    console.log(tsec);
     if ((tsec == 'null' || tsec != undefined || tsec == '') && req.query.$filter === 'productType==TT' )
 	   return res.json(dashboard_02);
-    else if ((tsec == 'null' || tsec != undefined || tsec == '') && req.query.$filter === 'productType==TT,contract==00743616562092377152')
-        return res.json(dashboard_02_si);
+    else if ((tsec == 'null' || tsec != undefined || tsec == '') && req.query.$filter === 'productType==SI,contract==00743616562092377152')
+        return res.json(dashboard_02_SI);
+    else if ((tsec == 'null' || tsec == undefined || tsec == '') && req.query.$filter === 'productType==IN,contract==00740010001368067104,currency==MXP')
+        return res.json(dashboard_03_IN);
     else if ((tsec != 'null' || tsec != undefined || tsec != '') && req.query.$filter === 'productType==TT')
         return res.json(dashboard_01);
 
