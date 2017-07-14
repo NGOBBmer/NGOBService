7593cbe02680ff926e7ce1af9b555aa020409aea
xmz2488 <giovanni.nicolas.contractor@bbva.com>
2017-07-14 10:02:01 -0500

    Fix (all): add agile empty2

--- routes/dashboard/dashboard.js
+++ routes/dashboard/dashboard.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var dashboard_01 = require('../../mock/V00/dashboard/dashboard/dashboard_01.json');
 var dashboard_02 = require('../../mock/V00/dashboard/dashboard/dashboard_02.json');
+var dashboard_03 = require('../../mock/V00/dashboard/dashboard/dashboard_03.json');
 var dashboard_02_SI = require('../../mock/V00/dashboard/dashboard/dashboard_02.SI.json');
 var dashboard_03_IN = require('../../mock/V00/dashboard/dashboard/dashboard_03.IN.json');
 var dashboard_04_IN = require('../../mock/V00/dashboard/dashboard/dashboard_04.IN.json');
@@ -24,13 +25,14 @@ router.use(function(req, res, next) {
 // handler for query http://localhost:5000/dashboard/V00/dashboard?$filter=productType==TT
 router.get('/V00/dashboard', function(req, res, next) {
     var tsec = req.headers['tsec'];
-    console.log(tsec);
-    if ((tsec == 'null' || tsec != undefined || tsec == '') && req.query.$filter === 'productType==TT' )
+    if ((tsec == 'null' || tsec == '') && req.query.$filter === 'productType==TT' )
 	   return res.json(dashboard_02);
     else if ((tsec == 'null' || tsec != undefined || tsec == '') && req.query.$filter === 'productType==SI,idContract==SIMXP0000001')
         return res.json(dashboard_02_SI);
     else if ((tsec == 'null' || tsec == undefined || tsec == '') && req.query.$filter === 'productType==IN,contract==INMXP0000001,currency==MXP')
         return res.json(dashboard_03_IN);
+    else if (tsec === '123456' && req.query.$filter === 'productType==TT')
+        return res.json(dashboard_03);
     else if ((tsec != 'null' || tsec != undefined || tsec != '') && req.query.$filter === 'productType==TT')
         return res.json(dashboard_01);
     else if ((tsec == 'null' || tsec == undefined || tsec == '') && req.query.$filter === 'productType==IN,contract==INMXP0000002,currency==MXP')
