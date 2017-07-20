ab2a4c3fff270de0a52d8c8863c7b5292594dda5
Paola Chavez <Paola@MacBook-Pro-de-Gonet.local>
2017-07-20 10:23:29 -0500

    Feat (all): update example json

--- routes/dashboard/mobileTransfers.js
+++ routes/dashboard/mobileTransfers.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var DATA_01 = require('../../mock/V00/dashboard/mobileTransfers/mobileTransfers.json');
 var ERROR = require('../../mock/V00/dashboard/mobileTransfers/mobileTransfersError.json');
-
+ 
 /* GET users listing. */
 router.use(function(req, res, next) {
     var host = req.get('origin');
@@ -26,7 +26,8 @@ router.post('/V00/dashboard/mobileTransfers/', function(req, res, next) {
 
 // handler for query http://localhost:4000/dashboard/V00/dashboard/mobileTransfers?amount=100&concept=concept&idAccount=AHMXP0000002
 router.get('/V00/dashboard/mobileTransfers', function(req, res, next) {
-  if(req.query.amount!=null && req.query.concept!= "" && req.query.idAccount !=""){
+  var otp = req.headers['otp'];
+  if(otp == '11111111' && req.query.amount!=null && req.query.concept!= "" && req.query.idAccount !=""){
     return res.json(DATA_01);
   }
   return res.status(400).json(ERROR);
