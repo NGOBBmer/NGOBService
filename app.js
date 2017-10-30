a3d20949163d10df1f085dfc62a98f4ea91ac38e
Paola Chavez <Paola@MacBook-Pro-de-Gonet.local>
2017-10-30 13:57:31 -0600

    Feat (all): update banks

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ var cashAdvanceFee = require('./routes/transfers/cashAdvanceFee');
 var mainTransfers = require('./routes/transfers/mainTransfers');
 var dashboardMovements = require('./routes/dashboard/dashboardMovements');
 var balanceDashboard = require('./routes/dashboard/balanceDashboard');
-var loadBanks = require('./routes/transfers/loadBanks');
+var loadBank = require('./routes/transfers/loadBanks');
 
 
 var app = express();
@@ -124,7 +124,7 @@ app.use('/transfers', cashAdvanceFee);
 app.use('/transfers', mainTransfers);
 app.use('/dashboard', dashboardMovements);
 app.use('/dashboard', balanceDashboard);
-app.use('/transfers', loadBanks);
+app.use('/transfers', loadBank);
 
 
 // catch 404 and forward to error handler
