25ad27c18125e7cee473967a270d71d2c576d3b3
Paola Chavez <Paola@MacBook-Pro-de-Gonet.local>
2017-10-30 12:45:40 -0600

    Feat (all): update example json loadBanks

--- routes/transfers/loadBanks.js
+++ routes/transfers/loadBanks.js
@@ -25,20 +25,7 @@ router.use(function(req, res, next) {
 // handler for query http://localhost:4000/transfers/V00/loadBanks?operationType=spei
 router.get('/V00/loadBanks', function(req, res, next) {
     var tsec = req.headers['tsec'];
-    if (tsec == 'null' || tsec == undefined && req.query.operationType==='spei' && req.query.cveBank==='90628')
-        return res.json(bankFound);
-    else if (tsec == 'null' || tsec == undefined && req.query.operationType==='otroscreditos' && req.query.cveBank==='30167')
-        return res.json(bankFound01);
-    else if (tsec == 'null' || tsec == undefined && req.query.operationType==='spei')
-        return res.json(banksCatalogSpei);
-    else if (tsec == 'null' || tsec == undefined && req.query.operationType==='clabe')
-        return res.json(banksCatalogClabe);
-    else if (tsec == 'null' || tsec == undefined && req.query.operationType==='tdd')
-        return res.json(banksCatalogTdd);
-    else if (tsec == 'null' || tsec == undefined && req.query.operationType==='tdc')
-        return res.json(banksCatalogTdc);
-    else if (tsec == 'null' || tsec == undefined && req.query.operationType==='otroscreditos')
-        return res.json(banksCatalogOtrosCreditos);
+    
 
     return res.status(400).json(banksError);
   next();
