91eebe46783e9172972f587f7b0934cb9757b2a0
Paola Chavez <Paola@MacBook-Pro-de-Gonet.local>
2018-01-09 11:12:51 -0600

    new json in dashboard

--- routes/dashboard/dashboardMovements.js
+++ routes/dashboard/dashboardMovements.js
@@ -32,11 +32,11 @@ router.get('/V00/dashboardMovements', function(req, res, next) {
     var tsec = req.headers['tsec'];
     if ( tsec == undefined && req.query.$filter === 'productType==SI,idContract==SIMXP0000003,period==0'){
        return res.json(dashMov_01_02);
-    }else if ((tsec == '' || tsec != 'null') && req.query.$filter === 'productType==SI,idContract==SIMXP0000001,period==0'){
+    }else if ((tsec == '' || tsec != 'null' || tsec === '2345678') && req.query.$filter === 'productType==SI,idContract==SIMXP0000001,period==0'){
        return res.json(dashMov_02_01);
-    } else if ((tsec == '' || tsec != 'null') && req.query.$filter === 'productType==SI,idContract==SIMXP0000001,period==1'){
+    } else if ((tsec == '' || tsec != 'null' || tsec === '2345678') && req.query.$filter === 'productType==SI,idContract==SIMXP0000001,period==1'){
        return res.json(dashMov_01_02_01);
-    } else if ((tsec == '' || tsec != 'null') && req.query.$filter === 'productType==SI,idContract==SIMXP0000001,period==2'){
+    } else if ((tsec == '' || tsec != 'null' || tsec === '2345678') && req.query.$filter === 'productType==SI,idContract==SIMXP0000001,period==2'){
        return res.json(dashMov_01_03);
     }else if (tsec == undefined && req.query.$filter === 'productType==SI,idContract==SIMXP0000005,period==0'){
        return res.json(dashMov_02_01);
