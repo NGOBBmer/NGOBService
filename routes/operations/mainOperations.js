45679a0293beec6a0a97609aa9c5a8083da70818
Paola Chavez <Paola@MacBook-Pro-de-Gonet.local>
2018-01-11 13:06:33 -0600

    Update deleteAgileOpe

--- routes/operations/mainOperations.js
+++ routes/operations/mainOperations.js
@@ -262,7 +262,9 @@ router.get('/V00/agileOperations', function(req, res, next) {
 // handler for query http://localhost:4000/operations/V00/deleteAgileOperations/PR0001?agileOperationType=RECURRING
 router.get('/V00/deleteAgileOperations/:id', function(req, res, next) {
   var tsec = req.headers['tsec'];
-  if(tsec === '123456' && req.query.agileOperationType === 'RECURRING')
+  if(req.params.id != '0001')
+    return res.json(OK);
+  else if(tsec === '123456' && req.query.agileOperationType === 'RECURRING')
    return res.status(409).json(DELETE_ERROR);
   if (req.query.agileOperationType === 'RECURRING' || req.query.agileOperationType === 'SCHEDULED')
   	return res.json(OK);
