06b0baac52c879a4278ee80b46557f2a7eee0e60
Paola Chavez <Paola@MacBook-Pro-de-Gonet.local>
2018-05-29 13:28:34 -0500

    Feat(all) update service createAgileOperations

--- routes/operations/mainOperations.js
+++ routes/operations/mainOperations.js
@@ -434,7 +434,7 @@ router.post('/V00/createAgileOperations', function(req, res, next) {
     }
     return res.json(createAgileOp01); 
   }else if (req.body.agileOperationType ==='QUICK'){
-     if (req.body.operationId !== '' || req.body.sender === undefined){
+     if (req.body.operationId == '' || req.body.sender === undefined){
       return res.status(400).json(createAgileOpErr_02); 
     }
     return res.json(createAgileOp01); 
